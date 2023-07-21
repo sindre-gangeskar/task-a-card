b57e2b886e6389ddbacce7974bded65519f7ccd7
Crucially-Ultrawide <80957863+Crucially-Ultrawide@users.noreply.github.com>
2023-07-21 19:27:33 +0200

    Added Additional Info functioniality

--- app.js
+++ app.js
@@ -15,24 +15,52 @@ class Card {
     //#endregion
 
     //#region Task Component
-
+    
+    /* Card elements */
     const content = (this.content = $("<div id='card-content' title='Hold to drag'></div>"));
     const taskContainer = (this.taskContainer = $("<div id='card-task-container'></div>"));
     const appendTaskInput = (this.input = $("<input id='card-input' placeholder='Enter Task' maxlength='14'></input>"));
     const taskItem = (this.taskItem = $(`<p id="task-item" ></p>`));
-
+    const detailsBtn = $((this.detailsBtn = $("<button class='details-btn'>. . .</button>")));
     const deleteBtn = (this.deleteBtn = $('<button class="btn delete-btn" role="button" data-bs-toggle="modal" data-bs-target="#modal-delete"></button'));
-    const deleteIcon = (this.deleteIcon = $('<i class="bi bi-x-lg delete-icon"></i>'));
+    const deleteIcon = (this.deleteIcon = $('<i class="bi bi-x delete-icon"></i>'));
 
+    /* Details modal */
+    const detailsModal = $((this.detailsModal = $("<div class='modal fade scrollable details-modal role='dialog' data-bs-backdrop='static'></div>")));
+    const detailsModalDialog = $((this.detailsModalDialog = $("<div class='modal-dialog modal-dialog-scrollable' role='document'></div>")));
+    const detailsModalContent = $((this.detailsModalContent = $("<div class='modal-content text-center'></div>")));
+    const detailsModalTitle = $((this.detailsModalTitle = $("<div class='modal-title p-3'><h5>Additional Information</h5></div>")));
+    const detailsModalBody = $((this.detailsModalBody = $("<div class='modal-body text-center'></div>")));
+    const detailsModalFooter = $((this.detailsModalFooter = $("<div class='modal-footer'><button class='btn btn-success' data-bs-dismiss='modal'>Save & Close</button></div>")));
+    const detailsModalTextArea = $((this.detailsModalTextArea = $("<textarea class='modalDetailsTextArea container-fluid w-100 p-3' placeholder='Enter additional information here' spellcheck='false'></textarea>")));
+
+    /* Card Container Appends */
     head.appendTo(container);
     titleContainer.appendTo(head);
     titleInput.appendTo(titleContainer);
+    detailsBtn.appendTo(head);
     content.appendTo(container);
     appendTaskInput.appendTo(container);
     deleteBtn.appendTo(head);
     deleteBtn.append(deleteIcon);
     container.appendTo($(".wrapper"));
     //#endregion
+
+    /* Details Modal Appends */
+    detailsModal.appendTo("body");
+    detailsModalDialog.appendTo(detailsModal);
+    detailsModalContent.appendTo(detailsModalDialog);
+    detailsModalTitle.appendTo(detailsModalContent);
+    detailsModalBody.appendTo(detailsModalContent);
+    detailsModalFooter.appendTo(detailsModalContent);
+    detailsModalTextArea.appendTo(detailsModalBody);
+
+    $(detailsBtn)
+      .closest(".details-btn")
+      .on("click", function () {
+        console.log("asdassads");
+        $(detailsModal).modal("show");
+      });
 
     $(".wrapper").sortable({
       placeholder: "marker",
@@ -282,7 +310,6 @@ function checkGroups(groups) {
     }
   }
   return false;
-
 }
 function addCardToGroup() {
   if (checkGroups(groups)) {
@@ -303,7 +330,6 @@ function addCardToGroup() {
   }
 }
 $(document).ready(function () {
-
   $("#add-card").on("click", function () {
     addCardToGroup();
   });
